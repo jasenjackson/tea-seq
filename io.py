f030f28a27f67eb4574f8897f4bf6911443b5519
EthanHolleman <eholleman@luc.edu>
2019-09-10 18:31:28 -0500

    Moved individual fasta checkers to depr_defs.py replaces with set based check

--- io.py
+++ io.py
@@ -4,27 +4,24 @@ Title: file input/output functions (io.py)
 Author: Jasen M. Jackson, Loyola '19
 This script contains definitions that handle file i/o
 '''
-import os.path
+import os
 
-def type_fa(path):
-    ## boolean: path suffix is '.fasta'
-    if path[-3:].lower() == ".fa":
-        return True
-    else: return False
+ALLOWED_FASTA = {'.fa', '.fasta', '.fastq'}
 
-def type_fasta(path):
-    if path[-6:].lower() == ".fasta":
-        return True
-    else: return False
 
 def type_fastq(path):
     if path[-6:].lower() == '.fastq':
         return True
-    else: return False
+    else:
+        return False
+
+
 def fasta_check(path):
-    ## check file for .fasta/.fa ending, get name of genome
-    fasta_check = type_fasta(path)
-    fa_check = type_fa(path)
-    if (fasta_check or fa_check):
+    ext = os.path.basename(path).split('.')[-1]
+    # get just basename of file then split at . to create two strings
+    # then just take the string after the . in ex test.fasta to
+    # return 'fasta'
+    if ext in ALLOWED_FASTA:
         return True
-    else: return False
+    else:
+        return False
