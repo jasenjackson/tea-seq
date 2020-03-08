cef2ff5cc872597bae75263a764ee23539560a1e
Ethan Holleman <eholleman@luc.edu>
2020-03-08 14:34:42 -0500

    Remove hardcode file paths and move functions that seem to be for building libraries to the library_builder.py file

--- library_filter.py
+++ library_filter.py
@@ -4,7 +4,8 @@ Title: Library filter functions (library_filter.py)
 Author: Jasen M. Jackson, Loyola '19
 This script contains functions necessary for filtering TRAPseq (teaseq???) libraries.
 '''
-import glob, os
+import glob
+import os
 import os.path
 import sys
 from io import *
@@ -12,119 +13,88 @@ from redundancy_map import *
 from feature_search import *
 from collections import defaultdict
 
-def merge_reads(r1,  r2, library_name, run_name, bin, data):
+from io import if_not_dir_make
 
-	#If contig has already been merged, skip this step.
-	if os.path.exists('results/'+run_name+"/"+library_name+'/out.extendedFrags.fastq'):
-		print("\tReads from "+library_name+' have already been merged. Skipping to next step!')
 
-	#otherwise merge forward and reverse read files for the library
-	else:
-		#run FLASH from the bin directory with 250bp maximum overlap AND interleaved output
-		output_directory = "results/"+run_name+"/"+library_name
-		print("\tMerging forward and reverse reads for " + library_name+"...")
-		os.system(bin + "/./flash " +data+"/"+r1+ " " +data+"/"+r2
-		+ " -d " + output_directory + " -M 250 --interleaved-output > results/"+
-		run_name+"/"+library_name+"/flash.log")
-		print("\t\tPair-end alignment console output directed to results/"+run_name+"/"+library_name+"/flash.log")
+def feature_trim(features, library_name, run_name, end_size):  # feature count, filture, trim
 
-def collate(library_name, run_name):
+    # open library FASTQ file & create trimmed file
+    file_path = "results/"+run_name+'/'+library_name+"/out.extendedFrags.fastq"
+    library_file = open(file_path, 'r')
+    print("\tTrimming "+library_name+" with provided feature sequences...")
+    trimmed_file_path = "results/"+run_name+"/" + \
+        library_name+"/"+library_name+".trimmed.fasta"
+    if os.path.exists(trimmed_file_path):
+        return 0
+    else:
+        trimmed_file = open(trimmed_file_path, 'a')
 
-	#If combined file has already been made, skip this step
-	if os.path.exists('results/'+run_name+'/'+library_name+'/'+library_name+".combined.fastq"):
-		print("\t"+library_name+' already has a combined fastq file. Skipping to next step!')
+        # iterate through every line in the library sequence file and compare to every filter sequence
+        has_adapter, has_element, has_killSequence, is_trimmedLine = False, False, False, False
+        trimmed_line = ""
+        count, adapter_pos, adapter_dist, element_pos, element_dist = 0, -1, -1, -1, -1
+        while True:  # for each line in the file
+            header = library_file.readline()
+            sequenceLine = library_file.readline()
+            plusLine = library_file.readline()
+            qualityScore = library_file.readline()
+            if not qualityScore:
+                break
+            has_adapter, has_element, has_killSequence = False, False, False
 
-	else:
-		print("\tCollating assembled and unassembled reads into combined file..")
-		#create file that combines assembled and unassembled reads
-		combined_file_path = "results/"+run_name+'/'+library_name+"/"+library_name+".combined.fastq"
-		combined_file = open(combined_file_path, 'a')
+            # interrogate sequenceLine for important sequences
+            for feat in features:
+                feature, threshold, type = feat[1], feat[2], feat[3]
+                if (type == "remove") and (feature in sequenceLine):
+                    has_killSequence = True
+                    break
+                # what ab RC adapter? Should be reverse orientation..
+                if (type == "adapter"):
+                    adapter_pos, adapter_dist = slidingSearch(
+                        sequenceLine, feature, 6, 2)
+                    adapter_len = len(feature)
+                    if (adapter_pos != -1):
+                        has_adapter = True
+                if (type == "element"):
+                    element_pos, element_dist = slidingSearch(
+                        sequenceLine, feature, 3, 1)
+                    if (element_pos != -1):
+                        has_element = True
 
-		#dump all of the contents of the assembled file into the combined file
-		assembled_file_path = "results/"+run_name+'/'+library_name+"/out.extendedFrags.fastq"
-		assembled_file = open(assembled_file_path, 'r')
-		for line in assembled_file.readlines():
-			combined_file.write(line)
-		assembled_file.close()
-		print("\t\t"+assembled_file_path+" was succesfully added to "+combined_file_path)
+            # trim and store eligible sequences
+            if ((has_killSequence == False) and (has_adapter == True) and (has_element == True)):
+                # print(count)
+                adapter_end = adapter_pos+adapter_len
+                trimmed_line = sequenceLine[adapter_end:element_pos] + '\n'
+                if len(trimmed_line) >= end_size:
+                    count += 1
+                    fasta_header = ">"+header
+                    new_entry = fasta_header+trimmed_line
+                    trimmed_file.write(new_entry)
 
-		#dump all of the contents of unassembled into combined files
-		unassembled_file_path = "results/"+run_name+'/'+library_name+"/out.notCombined.fastq"
-		unassembled_file = open(unassembled_file_path, 'r')
-		for line in unassembled_file.readlines():
-			combined_file.write(line)
-			unassembled_file.close()
-		print("\t\t"+unassembled_file_path+" was succesfully added to "+combined_file_path)
+        # print console output for trimmed reads process
+        features_used = ""
+        for feat in features:
+            type = feat[3]
+            if (type == "remove" or type == "adapter" or type == "element"):
+                features_used = features_used + feat[0] + ", "
+        features_used = features_used[:-2]
+        print("\t\t"+file_path +
+              " was succesfully trimmed and filtered using: " + features_used)
+        print("\t\t" + str(count) + " trimmed reads added to " + trimmed_file_path)
 
-def feature_trim(features, library_name, run_name, end_size): #feature count, filture, trim
-
-	#open library FASTQ file & create trimmed file
-	file_path = "results/"+run_name+'/'+library_name+"/out.extendedFrags.fastq"
-	library_file = open(file_path, 'r')
-	print("\tTrimming "+library_name+" with provided feature sequences...")
-	trimmed_file_path = "results/"+run_name+"/"+library_name+"/"+library_name+".trimmed.fasta"
-	if os.path.exists(trimmed_file_path): return 0
-	else:
-		trimmed_file = open(trimmed_file_path, 'a')
-
-		#iterate through every line in the library sequence file and compare to every filter sequence
-		has_adapter, has_element, has_killSequence, is_trimmedLine = False, False, False, False
-		trimmed_line = ""
-		count, adapter_pos, adapter_dist, element_pos, element_dist = 0, -1, -1, -1, -1
-		while True: #for each line in the file
-			header = library_file.readline()
-			sequenceLine = library_file.readline()
-			plusLine = library_file.readline()
-			qualityScore = library_file.readline()
-			if not qualityScore: break
-			has_adapter, has_element, has_killSequence = False, False, False
-
-			#interrogate sequenceLine for important sequences
-			for feat in features:
-				feature, threshold, type = feat[1], feat[2], feat[3]
-				if (type=="remove") and (feature in sequenceLine):
-					has_killSequence = True
-					break
-				if (type=="adapter"): # what ab RC adapter? Should be reverse orientation..
-					adapter_pos, adapter_dist = slidingSearch(sequenceLine, feature, 6, 2)
-					adapter_len = len(feature)
-					if (adapter_pos != -1): has_adapter = True
-				if (type=="element"):
-					element_pos, element_dist = slidingSearch(sequenceLine, feature, 3, 1)
-					if (element_pos != -1):
-						has_element = True
-
-			# trim and store eligible sequences
-			if ((has_killSequence==False) and (has_adapter==True) and (has_element==True)):
-				#print(count)
-				adapter_end = adapter_pos+adapter_len
-				trimmed_line = sequenceLine[adapter_end:element_pos] + '\n'
-				if len(trimmed_line) >= end_size:
-					count += 1
-					fasta_header = ">"+header
-					new_entry = fasta_header+trimmed_line
-					trimmed_file.write(new_entry)
-
-		#print console output for trimmed reads process
-		features_used = ""
-		for feat in features:
-			type = feat[3]
-			if (type=="remove" or type == "adapter" or type == "element"):
-				features_used = features_used + feat[0] + ", "
-		features_used = features_used[:-2]
-		print("\t\t"+file_path+" was succesfully trimmed and filtered using: "+ features_used)
-		print("\t\t"+ str(count) +" trimmed reads added to " +trimmed_file_path)
 
 def remove_duplicates(library_name, run_name, end_size):
 
-	## create redundancy map
-	trimmed_file_path = "results/"+run_name+'/'+library_name+"/"+library_name+".trimmed.fasta"
-	rm = redundancy_map(trimmed_file_path, end_size)
-	print("\t\tUnique reads: "+str(rm.unique_count))
-	print("\t\tDuplicate reads: "+str(rm.duplicate_count))
-	print("\t\tTotal reads: "+ str(rm.total_count))
-	rm.print_map_head(10)
-	'''## input/output function
+    # create redundancy map
+    trimmed_file_path = "results/"+run_name+'/' + \
+        library_name+"/"+library_name+".trimmed.fasta"
+    rm = redundancy_map(trimmed_file_path, end_size)
+    print("\t\tUnique reads: "+str(rm.unique_count))
+    print("\t\tDuplicate reads: "+str(rm.duplicate_count))
+    print("\t\tTotal reads: " + str(rm.total_count))
+    rm.print_map_head(10)
+    '''## input/output function
 	## open trimmed file & remove duplicates
 	trimmed_file_path = "results/"+run_name+'/'+library_name+"/"+library_name+".trimmed.fastq"
 
